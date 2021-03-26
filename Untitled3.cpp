2d2e6f08ea397beb4f453c58964b6e7ae1996843
akshitcoder009 <81291303+akshitcoder009@users.noreply.github.com>
2021-03-26 10:18:59 +0530

    addition program done

---
+++ Untitled3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>   
+using namespace std;
+
+int main ()
+ {
+ 	int x;
+ 	int a;
+ 	cout<< "enter first number";
+ 	cout<< endl;
+ 	cin>>x;
+ 	cout<< "enter second number";
+ 	cout<< endl;
+cin>>a;
+ 	int s;
+ 	cout<< "answer of addition";
+ 	cout<< endl;
+ 	s= x + a;
+ 	
+ 	cout<< s;
+ 	
+ 	cout<<endl;
+ 	cout<< "answer of subtraction";
+ 	cout<< endl;
+ 	s = x - a;
+ 	cout<< s;
+ 	cout<<endl;
+ 	cout<< "answer of multiplication";
+ 	cout<< endl;
+ 	s = x * a; 
+ 	cout<< s;
+ 	cout<< endl;
+ 	cout<< "answer of divide"; 
+ 	cout<< endl;
+ 	s = x / a;
+ 	cout<< s;
+ 	
+ 	
+ return 0;	
+ }
+ 
+ 
+ 
+  
