0707851bf649cd4c698e3a4524197b917752d090
akshitcoder009 <81291303+akshitcoder009@users.noreply.github.com>
2021-03-26 16:09:23 +0530

    i make a new programe conditions in c++

---
+++ votingSystem.cpp
@@ -0,0 +1,51 @@
+#include <iostream>   
+using namespace std;
+
+int main ()
+ {
+ 	
+ 	// a<b
+ 	//a <=b
+ 	// a>b
+ 	// a>=b  
+ 	// a==b
+ 	// a!=b
+ 	
+ 	
+ 	
+ 
+ 	int a;
+ 	int b;
+    cout<< "write first number";	
+ 	cout<< endl;
+ 	cin>>a;
+ 	cin>>b;
+ 	int result;
+	  result =a<b;
+	cout<< "first answer";
+	cout<< endl; 
+ 	cout<<result;
+ 	cout<< endl;
+ 	
+     result = a<=b;
+     cout<< "second answer";
+     cout<< endl;
+	 cout<< result;
+	 cout<< endl;
+	
+	 result = a==b;
+	 cout<< "third answer";
+	 cout<< endl;  
+     cout<< result;
+	 cout<< endl; 
+	 cout<< "fourth answer";
+	 cout<< endl;
+	 result = a!=b;
+	 cout<< result;
+	   	
+ return 0;	
+ }
+ 
+ 
+ 
+  
